abc1f232cca97e230f3edf02d4aebb5eadc81ba9
LeiZiKang <lzkhaoshua@icloud.com>
2023-04-03 21:02:42 +0800

    Initial Commit

---
+++ Demo2/Demo2App.swift
@@ -0,0 +1,67 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by 雷子康 on 2023/4/3.
+//
+
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    //MARK: - Linking App Delegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+//MARK: - Making it as Pure Menu Bar App
+
+
+//MARK: - Setting Up Menu Bar Icon and Menu Bar Popover area
+class AppDelegate: NSObject,ObservableObject,NSApplicationDelegate{
+    //MARK: Properties
+    @Published var statusItem: NSStatusItem?
+    @Published var popover = NSPopover()
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        setUpMacMenu()
+    }
+    
+    func setUpMacMenu(){
+        //MARK: Popover Prpperties
+        popover.animates = true
+        popover.behavior = .transient
+        
+        //MARK: - Linking SwiftUI View
+        popover.contentViewController = NSViewController()
+        popover.contentViewController?.view = NSHostingView(rootView: Home())
+        
+        //MARK: Making it as Key Window
+        popover.contentViewController?.view.window?.makeKey()
+        
+        //MARK: - Setting Menu Bar Icon and Action
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let menuButton = statusItem?.button{
+            menuButton.image = NSImage(named: "StatusIcon")
+            menuButton.action = #selector(menuButtonAction(sender: ))
+        }
+    }
+    
+    @objc func menuButtonAction(sender: AnyObject){
+        //MARK: - Showing/Closing Popover
+        if popover.isShown{
+            popover.performClose(sender)
+        }
+        else{
+            if let menuButton = statusItem?.button{
+                popover.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: .minY)
+            }
+        }
+    }
+}
+
+
